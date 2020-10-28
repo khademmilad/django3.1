63daf0fb1d9f0bdb74599d6bfb569354dd0da359
khademmilad <khademmilad@users.noreply.github.com>
2020-10-28 16:31:19 +0100

    urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('',views.index,name="index"),
     path('contact',views.contact,name="contact"),
     path('blog_create',views.blog_create_form),
-    path("detail/<int:my_id>",views.detail_view),
+    path("detail/<int:my_id>",views.detail_view,name='detail'),
     path("delete/<int:my_id>",views.delete_post)
 ]
