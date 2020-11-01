b0f554b2655ca862ac4efb9286fee43e2af27b48
khademmilad <khademmilad@users.noreply.github.com>
2020-11-01 14:03:19 +0100

    create comment and recomment models

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Comment,ReplyComment
 
-# Register your models here.
+
+admin.site.register(Comment)
+admin.site.register(ReplyComment)
--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from blog.models import Blog
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE,related_name="blog")
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="user")
+    comment = models.TextField(max_length=500)
+    time = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.blog.title
+
+    def block_comment(self):
+        if self.is_active == True:
+            self.is_active = False
+
+
+class ReplyComment(models.Model):
+    comment = models.ForeignKey(Comment,on_delete=models.CASCADE,related_name="recomment")
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    re_comment = models.TextField(max_length=500)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment.blog.title
